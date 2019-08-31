1c946ed8c343c34d12003b6a4b2f5854ff15c3c6
russell-archer <russell.archer@mac.com>
2019-08-31 13:58:48 +0100

    Initial Commit

---
+++ DarkModeDemo-UIKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DarkModeDemo-UIKit
+//
+//  Created by Russell Archer on 31/08/2019.
+//  Copyright © 2019 Russell Archer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
